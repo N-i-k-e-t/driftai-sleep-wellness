1d8b97f3a32c7860be82e476589c4088f08fcd38
Niket Patil <68462313+N-i-k-e-t@users.noreply.github.com>
2026-02-28 17:58:45 +0530

    Implement routing in App component

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Landing from './pages/Landing'
+import Dashboard from './pages/Dashboard'
+import Sounds from './pages/Sounds'
+import Tracker from './pages/Tracker'
+import Insights from './pages/Insights'
+import Profile from './pages/Profile'
+import Auth from './pages/Auth'
+import Layout from './components/Layout'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/auth" element={<Auth />} />
+      <Route element={<Layout />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/sounds" element={<Sounds />} />
+        <Route path="/tracker" element={<Tracker />} />
+        <Route path="/insights" element={<Insights />} />
+        <Route path="/profile" element={<Profile />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  )
+}
