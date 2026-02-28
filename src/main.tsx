4c5351e3d43fe8cfb6398fe39c7e27cd4afccd69
Niket Patil <68462313+N-i-k-e-t@users.noreply.github.com>
2026-03-01 02:07:01 +0530

    feat: wrap App with MobileOnly - blocks desktop, shows smartphone-only gate

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.tsx'
+import MobileOnly from './components/MobileOnly.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <MobileOnly>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </MobileOnly>
+  </StrictMode>,
 )
